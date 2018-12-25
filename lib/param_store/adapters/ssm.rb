3a836d0a1d3fa754d5b2d616f5c7a57e50c8785b
Pablo Cantero <pablohstc@gmail.com>
2018-12-25 16:23:05 -0500

    :lipstick:

--- lib/param_store/adapters/ssm.rb
+++ lib/param_store/adapters/ssm.rb
@@ -3,11 +3,12 @@ module ParamStore
     class SSM
       def fetch(key, *args, &block)
         tmp = {}
-        tmp[key] = ParamStore.ssm_client.get_parameter(name: key, with_decryption: true).value
-      rescue Aws::SSM::Errors::ParameterNotFound
-        # let the ensure below (tmp.fetch) deal with not found key and defaults
-      ensure
-        return tmp.fetch(key, *args, &block)
+        begin
+          tmp[key] = ParamStore.ssm_client.get_parameter(name: key, with_decryption: true).value
+        rescue Aws::SSM::Errors::ParameterNotFound
+          # let the tmp.fetch below deal with not found key and defaults
+        end
+        tmp.fetch(key, *args, &block)
       end
     end
   end
